e634965a433fc51455527cf24f5b95dccb318bca
omDroid <omdroid@omDroids-Mac-mini.local>
2018-06-08 21:33:17 +0430

    end of lesson 1

---
+++ SmackChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmackChat
+//
+//  Created by omDroid on 6/8/18.
+//  Copyright © 2018 iomDroid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
